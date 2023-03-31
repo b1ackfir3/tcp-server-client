f3d248417ae91a6e01592ae3d11fd610d8b837ce
= <anuputhussery23@gmail.com>
2023-03-31 15:38:35 +0530

    my third commit

---
+++ server.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+
+#include<netdb.h>
+#include<stdlib.h>
+#include<sys/socket.h>
+#include<unistd.h>
+#include<netinet.h>
+
+
+int main()
+{
+
+	int sockfd;
+	sockfd=socket(AF_INET,SOCK_STREAM,0);
+	
+	if(sock==-1)
+	{
+		printf("socket creation failed");
+	}
+	else
+	{
+		printf("socket creation successful");
+
+	}
+
+	return 0;
+}
